cdb347e2c2a6e7c2b22a3f0b8b37273b456da010
Matías Oubiña <matiasoubi@gmail.com>
2022-04-12 20:55:39 -0300

    Segunda Entrega

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import NavBar from "./NavBar"
+
+const Header = () => {
+    return(
+        <header>
+            <div className="logo_header">
+                <h1>JCK FOOD</h1>
+            </div>
+            <div className="navBar_header">
+                <NavBar/>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = () =>{
+    return(
+        <nav className="navBar">
+            <a href="#">Inicio</a>
+            <a href="#">Nosotros</a>
+            <a href="#">Mis Reservas</a>
+            <a href="#">Mis Pedidos</a>
+        </nav>
+    )
+}
+
+export default NavBar
